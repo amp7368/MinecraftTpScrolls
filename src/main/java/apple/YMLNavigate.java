368463ebf8f46ec8e3e62747be404167b0f0ddae
amp7368 <41219847+amp7368@users.noreply.github.com>
2020-03-05 16:50:13 -0500

    refactored and added navigation strings to ScrollAddAllCommand via YMLNavigate

---
+++ src/main/java/apple/YMLNavigate.java
@@ -0,0 +1,10 @@
+package apple;
+
+public class YMLNavigate {
+    public static final String INVENTORY = "inventory";
+    public static final String INVENTORY_ALL = "all";
+    public static final String ITEM = "item";
+    public static final String MATERIAL = "material";
+    public static final String LORE = "lore";
+    public static final String LINE ="line" ;
+}
